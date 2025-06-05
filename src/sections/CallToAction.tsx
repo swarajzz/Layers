3fede86352e3528a8263194b125280fb6aac81d3
swarajzz <sharma.swaraj0403@gmail.com>
2025-06-05 11:09:38 +0530

    add CTA section

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,3 +1,18 @@
 export default function CallToAction() {
-    return <div>Call To Action</div>;
+    return (
+        <section className="py-24">
+            <div className="flex overflow-x-clip p-4">
+                <div className="flex flex-none gap-16 text-7xl font-medium md:text-8xl">
+                    {Array.from({ length: 10 }).map((_, i) => (
+                        <div key={i} className="flex items-center gap-16">
+                            <span className="text-7xl text-lime-400">
+                                &#10038;
+                            </span>
+                            <span>Try it for free</span>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
 }
